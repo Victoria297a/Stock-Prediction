1a5f1f1ad163c8693e19d1d28ed67268c5b74399
Victoria297a <vikkiv2k18@gmail.com>
2026-08-11 17:23:29 +0300

    Add initial implementation of stock prediction using ThetaForecaster

---
+++ prediction/future.py
@@ -0,0 +1,23 @@
+from matplotlib.pyplot import plot
+from sktime.datasets import load_airline
+from sktime.forecasting.base import ForecastingHorizon
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.forecasting.theta import ThetaForecaster
+from sktime.performance_metrics.forecasting import mean_absolute_percentage_error
+
+import pandas as pd
+
+y = load_airline()
+"""print(y)
+"""
+y = pd.DataFrame(load_airline())
+y.plot() 
+y_train, y_test = temporal_train_test_split(y, test_size = 0.8)
+fh = ForecastingHorizon(y_test.index, is_relative=False)
+"""print(fh)"""
+forecaster = ThetaForecaster(sp=12)
+forecaster.fit(y_train)
+y_pred = forecaster.predict(fh)
+"""print(y_pred)"""
+#print(mean_absolute_percentage_error(y_test, y_pred))
+print(plot(y, y_pred))
